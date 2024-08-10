bf786b26d2ced6e5911befdf1813be2a37415811
Sam Wen <samswen@gmail.com>
2024-08-10 18:17:18 +0000

    update

--- types.index.d.ts
+++ types.index.d.ts
@@ -1,5 +1,7 @@
 
 declare module '@xinferai/help-utils' {
+    export function isInBrowser(): boolean;
+    export function isPlainObject(obj: any): boolean;
     export function secondsToHumanReadable(seconds: number): string;
     export function toSnakeCase(str: string): string;
     export function camelToSnake(obj: any): any;
